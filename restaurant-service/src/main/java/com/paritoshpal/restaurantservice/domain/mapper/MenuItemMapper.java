67830ce304d9995bf4e3f2845a2f7ee9118b2c30
ram063527 <paritoshpal7796@gmail.com>
2026-02-06 21:31:19 +0000

    MenuItemMapper

--- restaurant-service/src/main/java/com/paritoshpal/restaurantservice/domain/mapper/MenuItemMapper.java
+++ restaurant-service/src/main/java/com/paritoshpal/restaurantservice/domain/mapper/MenuItemMapper.java
@@ -1,4 +1,33 @@
 package com.paritoshpal.restaurantservice.domain.mapper;
 
+import com.paritoshpal.restaurantservice.domain.MenuItemEntity;
+import com.paritoshpal.restaurantservice.domain.models.CreateMenuItemRequest;
+import com.paritoshpal.restaurantservice.domain.models.MenuItemResponse;
+import com.paritoshpal.restaurantservice.domain.models.MenuItemUpdateRequest;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+
+
+
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface MenuItemMapper {
+
+    MenuItemResponse toMenuItemResponse(MenuItemEntity menuItem);
+
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "menu", ignore = true) // Handle in service layer
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    MenuItemEntity toMenuItemEntity(CreateMenuItemRequest request);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "menu", ignore = true) // Handle in service layer
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateMenuItemFromRequest(MenuItemUpdateRequest request, @MappingTarget MenuItemEntity menuItem);
+
 }
