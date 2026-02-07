103b15abca075bf996d31cec9a537b7f6226842f
ram063527 <paritoshpal7796@gmail.com>
2026-02-07 16:38:39 +0000

    Global Exception Handler

---
+++ restaurant-service/src/main/java/com/paritoshpal/restaurantservice/web/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,125 @@
+package com.paritoshpal.restaurantservice.web.exceptions;
+
+import com.paritoshpal.restaurantservice.domain.exceptions.*;
+import jakarta.annotation.Nullable;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.*;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.net.URI;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private static final URI NOT_FOUND_TYPE = URI.create("https://api.foodDelivery.com/errors/not-found");
+    private static final URI ISE_FOUND_TYPE = URI.create("https://api.foodDelivery.com/errors/server-error");
+    private static final URI BAD_REQUEST_TYPE = URI.create("https://api.foodDelivery.com/errors/bad-request");
+    private static final URI CONFLICT_TYPE = URI.create("https://api.foodDelivery.com/errors/conflict");
+
+
+    static final String SERVICE_NAME = "restaurant-service";
+
+    @ExceptionHandler(Exception.class)
+    ProblemDetail handleUnhandledException(Exception e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+        problemDetail.setTitle("Internal Server Error");
+        problemDetail.setType(ISE_FOUND_TYPE);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(EmailAlreadyInUseException.class)
+    ProblemDetail handleEmailAlreadyInUseException(EmailAlreadyInUseException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.CONFLICT, e.getMessage());
+        problemDetail.setTitle("Email Already In Use");
+        problemDetail.setType(CONFLICT_TYPE);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Validation");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MenuItemNotFoundException.class)
+    ProblemDetail handleMenuItemNotFoundException(MenuItemNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+        problemDetail.setTitle("Menu Item Not Found");
+        problemDetail.setType(NOT_FOUND_TYPE);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(MenuNotFoundException.class)
+    ProblemDetail handleMenuNotFoundException(MenuNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+        problemDetail.setTitle("Menu Not Found");
+        problemDetail.setType(NOT_FOUND_TYPE);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(RestaurantAddressNotFoundException.class)
+    ProblemDetail handleRestaurantAddressNotFoundException(RestaurantAddressNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+        problemDetail.setTitle("Restaurant Address Not Found");
+        problemDetail.setType(NOT_FOUND_TYPE);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+    @ExceptionHandler(RestaurantNotFoundException.class)
+    ProblemDetail handleRestaurantNotFoundException(RestaurantNotFoundException e) {
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, e.getMessage());
+        problemDetail.setTitle("Restaurant Not Found");
+        problemDetail.setType(NOT_FOUND_TYPE);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Generic");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return problemDetail;
+    }
+
+
+    @Override
+    @Nullable
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String errorMessage = error.getDefaultMessage();
+            errors.add(errorMessage);
+        });
+
+        ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Invalid request payload");
+        problemDetail.setTitle("Bad Request");
+        problemDetail.setType(BAD_REQUEST_TYPE);
+        problemDetail.setProperty("errors", errors);
+        problemDetail.setProperty("service", SERVICE_NAME);
+        problemDetail.setProperty("error_category", "Validation");
+        problemDetail.setProperty("timestamp", Instant.now());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problemDetail);
+
+
+    }
+
+
+
+}
